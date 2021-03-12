f1843af1a6e5e621437ee5c28e87f40f470ca28c
Edward Liveikis <ed@ardentblue.com>
2021-03-12 11:10:20 +0100

    Added dry run flag, put directory cleaning on goroutine, added timer.

--- main.go
+++ main.go
@@ -1,97 +1,114 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/fs"
 	"os"
+	"sync"
 	"time"
 )
 
-var dryRun = false
+const separator = string(os.PathSeparator)
+const verbose = false
+
+var isDryRun *bool
 
 func removeFile(name string) {
 	var err error
-	if !dryRun {
+	if !*isDryRun {
 		err = os.Remove(name)
 	}
 	if err != nil {
 		fmt.Println(fmt.Sprintf("error removing file %s. error: %s", name, err.Error()))
-	} else {
+	} else if verbose {
 		fmt.Println("removed file", name)
 	}
 }
 
-func cleanFiles(cutoff time.Time, files []fs.DirEntry) {
+func cleanFiles(wg *sync.WaitGroup, cutoff time.Time, files []fs.DirEntry, path string) {
 	for _, f := range files {
 		info, err := f.Info()
 		if err != nil {
 			panic(err.Error())
 		}
-		path := info.Name()
+		filename := info.Name()
 
-		if info.ModTime().After(cutoff) {
-			fmt.Println("keeping", path)
-			continue
-		}
-
-		// If it's a file, remove it.
+		// Handle file.
 		if !info.IsDir() {
-			removeFile(path)
+			if info.ModTime().After(cutoff) {
+				if verbose {
+					fmt.Println("keeping", filename)
+				}
+				continue
+			}
+			removeFile(path + separator + filename)
 			continue
 		}
 
-		// Enter directory, clean it, and cd back up to cwd.
-		err = os.Chdir(path)
-		if err != nil {
-			fmt.Println(fmt.Sprintf("error entering dir %s. error: %s", path, err.Error()))
-			continue
-		}
-		cleanCurrentDir(cutoff)
-		err = os.Chdir("./..")
-		if err != nil {
-			panic(err.Error())
-		}
+		// Clean sub directory.
+		subPath := path + separator + filename
+		fmt.Println("cleaning sub directory", subPath)
+		wg.Add(1)
+		go cleanDir(wg, cutoff, subPath)
 
-		// Check if directory is now empty and remove it if so.
-		dirFiles, err := os.ReadDir(path)
+		// Check if sub directory is now empty and remove it if so.
+		dirFiles, err := os.ReadDir(subPath)
 		if err != nil {
-			fmt.Println(fmt.Sprintf("error removing dir %s. error: %s", path, err.Error()))
+			fmt.Println(fmt.Sprintf("error removing dir %s. error: %s", subPath, err.Error()))
 			continue
 		}
 		if len(dirFiles) != 0 {
 			continue
 		}
-		if !dryRun {
-			err = os.Remove(path)
+		if !*isDryRun {
+			err = os.Remove(subPath)
 		}
 		if err != nil {
-			fmt.Println(fmt.Sprintf("error removing dir %s. error: %s", path, err.Error()))
+			fmt.Println(fmt.Sprintf("error removing dir %s. error: %s", subPath, err.Error()))
 			continue // Clean as much as possible.
 		}
-		fmt.Println("removed dir", path)
+		fmt.Println("removed dir", subPath)
 	} // Files.
 }
 
-func cleanCurrentDir(cutoff time.Time) {
-	files, err := os.ReadDir(".")
+func cleanDir(wg *sync.WaitGroup, cutoff time.Time, path string) {
+	defer wg.Done()
+	files, err := os.ReadDir(path)
 	if err != nil {
 		panic(err.Error())
 	}
-	cleanFiles(cutoff, files)
+	cleanFiles(wg, cutoff, files, path)
 }
 
 // Removes all files and folders from windows appdata/local/temp folder that are
 // older than 48 hours old (no specific considerations for daylight savings
 // time transitions).
 func main() {
-	tempDir := os.TempDir()
-	fmt.Println(tempDir)
+	// Parse flags.
+	isDryRun = flag.Bool("dry", false, "A dry run will only print out file removal actions, not perform them.")
+	flag.Parse()
+	if *isDryRun {
+		fmt.Println("--------------------------------------------------------------")
+		fmt.Println("dry run enabled")
+	}
 
+	// Navigate to temp folder.
+	tempDir := os.TempDir()
 	err := os.Chdir(tempDir)
 	if err != nil {
 		panic(err.Error())
 	}
+	fmt.Println("--------------------------------------------------------------")
+	fmt.Println("cleaning", tempDir)
 
-	cutoff := time.Now().Add(-time.Hour * 48)
-	cleanCurrentDir(cutoff)
+	startTime := time.Now()
+	cutoff := startTime.Add(-time.Hour * 48)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go cleanDir(&wg, cutoff, ".")
+	wg.Wait()
+
+	fmt.Println("--------------------------------------------------------------")
+	fmt.Println("completed in", time.Now().Sub(startTime))
 }
