a7f64cad0216c8aecb7c3942db30f6129769ddf9
Edward Liveikis <ed@ardentblue.com>
2021-03-12 10:28:22 +0100

    Directory recursion to ensure file modified dates are checked directly.

--- main.go
+++ main.go
@@ -2,9 +2,83 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
 	"time"
 )
+
+var dryRun = false
+
+func removeFile(name string) {
+	var err error
+	if !dryRun {
+		err = os.Remove(name)
+	}
+	if err != nil {
+		fmt.Println(fmt.Sprintf("error removing file %s. error: %s", name, err.Error()))
+	} else {
+		fmt.Println("removed file", name)
+	}
+}
+
+func cleanFiles(cutoff time.Time, files []fs.DirEntry) {
+	for _, f := range files {
+		info, err := f.Info()
+		if err != nil {
+			panic(err.Error())
+		}
+		path := info.Name()
+
+		if info.ModTime().After(cutoff) {
+			fmt.Println("keeping", path)
+			continue
+		}
+
+		// If it's a file, remove it.
+		if !info.IsDir() {
+			removeFile(path)
+			continue
+		}
+
+		// Enter directory, clean it, and cd back up to cwd.
+		err = os.Chdir(path)
+		if err != nil {
+			fmt.Println(fmt.Sprintf("error entering dir %s. error: %s", path, err.Error()))
+			continue
+		}
+		cleanCurrentDir(cutoff)
+		err = os.Chdir("./..")
+		if err != nil {
+			panic(err.Error())
+		}
+
+		// Check if directory is now empty and remove it if so.
+		dirFiles, err := os.ReadDir(path)
+		if err != nil {
+			fmt.Println(fmt.Sprintf("error removing dir %s. error: %s", path, err.Error()))
+			continue
+		}
+		if len(dirFiles) != 0 {
+			continue
+		}
+		if !dryRun {
+			err = os.Remove(path)
+		}
+		if err != nil {
+			fmt.Println(fmt.Sprintf("error removing dir %s. error: %s", path, err.Error()))
+			continue // Clean as much as possible.
+		}
+		fmt.Println("removed dir", path)
+	} // Files.
+}
+
+func cleanCurrentDir(cutoff time.Time) {
+	files, err := os.ReadDir(".")
+	if err != nil {
+		panic(err.Error())
+	}
+	cleanFiles(cutoff, files)
+}
 
 // Removes all files and folders from windows appdata/local/temp folder that are
 // older than 48 hours old (no specific considerations for daylight savings
@@ -13,38 +87,11 @@ func main() {
 	tempDir := os.TempDir()
 	fmt.Println(tempDir)
 
-	files, err := os.ReadDir(tempDir)
-	if err != nil {
-		panic(err.Error())
-	}
-	err = os.Chdir(tempDir)
+	err := os.Chdir(tempDir)
 	if err != nil {
 		panic(err.Error())
 	}
 
 	cutoff := time.Now().Add(-time.Hour * 48)
-	for _, f := range files {
-		info, err := f.Info()
-		if err != nil {
-			panic(err.Error())
-		}
-		if info.ModTime().After(cutoff) {
-			fmt.Println("keeping", info.Name())
-			continue
-		}
-		fmt.Println("deleting", info.Name())
-		if info.IsDir() {
-			err = os.RemoveAll(f.Name())
-			if err != nil {
-				fmt.Println(fmt.Sprintf("error removing dir %s. error: %s", info.Name(), err.Error()))
-				// Continue, try to remove as many as possible.
-			}
-		} else {
-			err = os.Remove(f.Name())
-			if err != nil {
-				fmt.Println(fmt.Sprintf("error removing file %s. error: %s", info.Name(), err.Error()))
-				// Continue, try to remove as many as possible.
-			}
-		}
-	}
+	cleanCurrentDir(cutoff)
 }
